275cf769ef91e0d8553cad02da45a85d84703a9b
Matthias Mullie <git@mullie.eu>
2013-04-11 11:02:06 +0200

    Cleanup infowindow html/css

--- server/ajax/location.php
+++ server/ajax/location.php
@@ -17,37 +17,35 @@ $prepareData->execute(array(':id' => $id));
 $data = $prepareData->fetch();
 
 if ( $data !== false ) {
-	echo
-		'<div id="infowindowMarker">
+	echo '
+		<div id="infowindowMarker">
 			<div id="infowindowTop" class="clearfix">
 				<a href="' . $data['url'] . '">
 					<h2>' . $data['title'] . '</h2>
 					<p>' . str_repeat('&#9733;', (int) $data['stars']) . '</p>
 				</a>
 			</div>
-			<div id="infowindowData">
-				<div id="infowindowContent">
-					<a id="markerUrl" class="clearfix" href="' . $data['url'] . '"><span class="leftSpan">Bestel</span> <span class="rightSpan">€' . $data['price'] . '</span></a>
-					<p id="markerText">' . $data['text'] . '</p>
-				</div>
-			</div>
-			<div id="infowindowBottom">
-				<p id="markerDisclaimer"><a class="infowindow" href="/server/disclaimer.php?url='. urlencode( '/server/ajax/location.php?id='.$id ) .'">Disclaimer</a></p>
+			<div id="infowindowContent">
+				<a id="markerUrl" class="clearfix" href="' . $data['url'] . '"><span class="leftSpan">Bestel</span> <span class="rightSpan">€' . $data['price'] . '</span></a>
+				<p id="markerText">' . $data['text'] . '</p>
 			</div>
 			<div id="markerImage" style="background-image: url(' . $data['image'] . ')">
 				<a href="' . $data['url'] . '"></a>
 			</div>
+		</div>
+		<div id="infowindowBottom">
+			<p id="markerDisclaimer"><a class="infowindow" href="/server/disclaimer.php?url='. urlencode( '/server/ajax/location.php?id='.$id ) .'">Disclaimer</a></p>
 		</div>';
 } else {
-	echo
-	'<div id="infowindowContent">
-		<h2>Fout!</h2>
-		<p>We konden geen gegevens voor de gevraagde locatie ophalen.</p>
-		<p>
-			Dit probleem kan zich voordoen wanneer we onze locatie-database aan het updaten zijn met de laatste promoties.
-			In dit geval zal u binnenkort wel over de gevraagde informatie kunnen beschikken, en raden we u aan om straks opnieuw te proberen.
-		</p>
-		<p>Mocht u dit probleem blijven ervaren, gelieve contact met ons op te nemen via ons <a class="infowindow" href="/server/contact.php" title="contacteer last-minute-vakanties.be">contactformulier</a>.</p>
-		<p>Alvast onze excuses!</p>
-	</div>';
+	echo '
+		<div id="infowindowContent">
+			<h2>Fout!</h2>
+			<p>We konden geen gegevens voor de gevraagde locatie ophalen.</p>
+			<p>
+				Dit probleem kan zich voordoen wanneer we onze locatie-database aan het updaten zijn met de laatste promoties.
+				In dit geval zal u binnenkort wel over de gevraagde informatie kunnen beschikken, en raden we u aan om straks opnieuw te proberen.
+			</p>
+			<p>Mocht u dit probleem blijven ervaren, gelieve contact met ons op te nemen via ons <a class="infowindow" href="/server/contact.php" title="contacteer last-minute-vakanties.be">contactformulier</a>.</p>
+			<p>Alvast onze excuses!</p>
+		</div>';
 }
